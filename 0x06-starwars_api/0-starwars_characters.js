773db942c9a53982ed10cfa365877460bd12f404
Terry <terrybmochire@gmail.com>
2023-02-10 11:21:58 +0300

    Task.

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,28 +1,28 @@
-#!/Users/terry/.nvm/versions/node/v18.12.1/bin/node
+#!/usr/bin/node
 
 const request = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2] + '/';
 
 request(url, function (error, response, body) {
-    if (error) {
-        console.log(error);
-    } else {
-        const data = JSON.parse(body);
-        const characters = data.characters;
-        console.log(characters);
-        printCharacters(characters);
-    }
+  if (error) {
+    console.log(error);
+  } else {
+    const data = JSON.parse(body);
+    const characters = data.characters;
+    console.log(characters);
+    printCharacters(characters);
+  }
 });
 
-function printCharacters(characters) {
-    for (let i = 0; i < characters.length; i++) {
-        request(characters[i], function (error, response, body) {
-            if (error) {
-                console.log(error);
-            } else {
-                const data = JSON.parse(response.body);
-                console.log(data.name);
-            }
-        });
-    }
+function printCharacters (characters) {
+  for (let i = 0; i < characters.length; i++) {
+    request(characters[i], function (error, response, body) {
+      if (error) {
+        console.log(error);
+      } else {
+        const data = JSON.parse(response.body);
+        console.log(data.name);
+      }
+    });
+  }
 }
